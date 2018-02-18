9e28241254c97d417a35206064785db2977ecd3e
Smodi16 <99smodi@gmail.com>
2018-02-18 13:42:00 +0530

    ohhhhooooo

--- app/src/main/java/com/example/user/hackk/Imagess.java
+++ app/src/main/java/com/example/user/hackk/Imagess.java
@@ -1,30 +1,31 @@
+
 package com.example.user.hackk;
 
 
 
 import android.content.Intent;
-        import android.os.AsyncTask;
-        import android.os.Bundle;
-        import android.support.v7.app.AppCompatActivity;
-        import android.view.View;
-        import android.widget.Button;
-        import android.widget.ImageView;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 /**
  * Created by user on 16-02-2018.
  */
 
-        import android.support.v7.app.AppCompatActivity;
-        import android.os.Bundle;
-        import android.util.Log;
-        import android.widget.ArrayAdapter;
-        import android.widget.ImageView;
-        import android.widget.ListView;
-        import android.widget.TextView;
-        import android.widget.Toast;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
-        import java.net.MalformedURLException;
+import java.net.MalformedURLException;
 //import java.net.URL;
 
 
@@ -44,7 +45,7 @@ public class Imagess extends AppCompatActivity {
         // Toast.makeText(this, ""+code, Toast.LENGTH_SHORT).show();
         //TextView textView=(TextView)findViewById(R.id.text);
         //TextView textView=findViewById(R.id.text);
-         ImageView imageView=findViewById(R.id.imageview);
+        ImageView imageView=findViewById(R.id.imageview);
         button=findViewById(R.id.btn1);
        /* imageView.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,13 +57,8 @@ public class Imagess extends AppCompatActivity {
         });*/
 
         button.setOnClickListener(new View.OnClickListener() {
-            @Override
+            url3 ur = new url3(Imagess.this);
             public void onClick(View v) {
-                Intent inten=new Intent(Imagess.this,Nescafe.class);
-                inten.putExtra("qr",code);
-                startActivity(inten);
-                url3 ur = new url3(Imagess.this);
-                // TextView textView=(TextView)findViewById(R.id.text);
                 try{
 
                     //s=ur.execute("http://192.168.43.200/docs/saloni1.php").get();
@@ -70,6 +66,12 @@ public class Imagess extends AppCompatActivity {
 
                     // Log.d(TAG,s);
                 }catch(Exception IO){}
+
+                Intent inten=new Intent(Imagess.this,Nescafe.class);
+                inten.putExtra("qr",code);
+                startActivity(inten);
+
+                // TextView textView=(TextView)findViewById(R.id.text);
 
 
 
---
+++ app/src/main/java/com/example/user/hackk/url2.java
@@ -0,0 +1,77 @@
+package com.example.user.hackk;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+import com.squareup.picasso.Picasso;
+
+import java.io.IOException;
+
+/**
+ * Created by user on 18-02-2018.
+ */
+class url2 extends AsyncTask<String,Void,String>
+{
+    Context mContext;
+    TextView mTextView;
+    Button btn;
+    String code;
+    //ListView listView;
+    int id ;
+    ImageView imageView;
+
+    url2(Context context)
+    {
+        mContext=context;
+       // this.btn=button;
+
+    }
+    Request request;
+    @Override
+    protected String doInBackground(String... strings) {
+        String result = null;
+        String url = strings[0];
+
+        // code=strings[0];
+        OkHttpClient okHttpClient = new OkHttpClient();
+        request = new Request.Builder().url(url).build();
+        Response response = null;
+        try
+        {
+            response=okHttpClient.newCall(request).execute();
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        try
+        {
+            result=response.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        String output1[]=null;
+        try {
+            output1 = s.split("#111#");
+            Toast.makeText(mContext, "you had a successful exit", Toast.LENGTH_SHORT).show();
+            // mTextView.setText(output1[id]);
+
+            //Picasso.with(mContext).load(output1[0].into(imageView);
+        }catch (NullPointerException e)
+        {
+//            Toast.makeText(mContext, "Slow Internet", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
